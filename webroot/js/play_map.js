7e72d57a0d96485f09d93f2a9ea01b1083a6b89c
John Liotta <john.liotta@gmail.com>
2017-03-19 14:23:00 -0500

    beginning of notifications, layout for working on radius slider/leaflet

--- webroot/js/play_map.js
+++ webroot/js/play_map.js
@@ -76,20 +76,22 @@ $('document').ready(function() {
 
     // radius functionality
     var radiusInput = document.getElementById('radius');
-
+    var radius = 50;
+    console.log(radius);
 
     radiusInput.onchange = function() {
-            console.log('Radius: ' + radiusInput.value);
-            radius = radiusInput.value
-        }
-        // create event circle
+        console.log('Radius: ' + radiusInput.value);
+        radius = radiusInput.value
+    }
+    console.log(radius);
+    // create event circle
     var circle = L.circle(35.2226, -95.4395, {
         color: 'rgb(61, 182, 239)',
         fillColor: 'rgb(61, 182, 239)',
         fillOpacity: 0.5,
-        radius: 500000
+        radius: radius
     });
-
+    console.log(circle);
     // svg tornado marker
     var tornadoSVG = "<svg version='1.1' id='tornado' class='climacon climacon_tornado leaflet-marker-icon leaflet-zoom-animated leaflet-interactive leaflet-marker-draggable' viewBox='15 15 70 70'><g class='climacon_iconWrap climacon_iconWrap-tornado'><g class='climacon_componentWrap climacon_componentWrap-tornado'><path class='climacon_component climacon_component-stroke climacon_component-stroke_tornadoLine'd='M68.997,36.459H31.002c-1.104,0-2-0.896-2-1.999c0-1.104,0.896-2,2-2h37.995c1.104,0,2,0.896,2,2C70.997,35.563,70.102,36.459,68.997,36.459z'></path><path class='climacon_component climacon_component-stroke climacon_component-stroke_tornadoLine' d='M35.002,40.459h29.996c1.104,0,2,0.896,2,2s-0.896,1.999-2,1.999H35.002c-1.104,0-2-0.896-2-1.999C33.002,41.354,33.898,40.459,35.002,40.459z'></path><path class='climacon_component climacon_component-stroke climacon_component-stroke_tornadoLine' d='M39.001,48.458h21.998c1.104,0,1.999,0.896,1.999,1.999c0,1.104-0.896,2-1.999,2H39.001c-1.104,0-1.999-0.896-1.999-2C37.002,49.354,37.897,48.458,39.001,48.458z'></path><path class='climacon_component climacon_component-stroke climacon_component-stroke_tornadoLine' d='M47,64.456h5.999c1.104,0,2,0.896,2,1.999s-0.896,2-2,2H47c-1.104,0-2-0.896-2-2S45.896,64.456,47,64.456z'></path><path class='climacon_component climacon_component-stroke climacon_component-stroke_tornadoLine'd='M40.869,58.456c0-1.104,0.896-1.999,2-1.999h13.998c1.104,0,2,0.896,2,1.999c0,1.104-0.896,2-2,2H42.869C41.765,60.456,40.869,59.561,40.869,58.456z'></path></g></g></svg>";
 
@@ -123,14 +125,14 @@ $('document').ready(function() {
                             color: 'rgb(61, 182, 239)',
                             fillColor: 'rgb(61, 182, 239)',
                             fillOpacity: 0.5,
-                            radius: 500000
+                            radius: radius
                         });
 
                         circle.addTo(map);
                         circle.bindPopup("Lat, Lon : " + latToFixed + ", " + lngToFixed).openPopup();
 
-                        var radius = circle.getRadius();
-
+                        //var radius = circle.getRadius();
+                        
                     } else {
                         circle.setLatLng(e.latlng);
                         circle.bindPopup("Lat, Lon : " + latToFixed + ", " + lngToFixed).openPopup();
