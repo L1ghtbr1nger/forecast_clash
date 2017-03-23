5b4829666774e3fca32edef233a0fcdc445253f3
Jason Kaplan <jason.kaplan88@gmail.com>
2017-03-23 04:03:05 -0500

    Click map to zoom on heatmap added

--- webroot/js/heatmaps.js
+++ webroot/js/heatmaps.js
@@ -1,6 +1,13 @@
 // initialize leaflet map 
 var map = L.map('map', {
-    doubleClickZoom: false
+    doubleClickZoom: true,
+    scrollWheelZoom: false
+});
+
+map.once('focus', function() { map.scrollWheelZoom.enable(); });
+
+map.on('click', function() {
+    map.scrollWheelZoom.enable();
 });
 
 map.setView([35.2226, -97.4395], 5);
@@ -10,12 +17,12 @@ L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
 }).addTo(map);
 
-var legend2 = L.control({position: 'topright'}); 
-legend2.onAdd = function (map) {        
+var legend2 = L.control({ position: 'topright' });
+legend2.onAdd = function(map) {
     var div = L.DomUtil.create('div', 'info legend2 map-logo');
-    div.innerHTML = '<img src="../img/logo-light-blue-sm.png" style="width:125px; ">';     
+    div.innerHTML = '<img src="../img/logo-light-blue-sm.png" style="width:125px; ">';
     return div;
-};      
+};
 legend2.addTo(map);
 
 //heatmap layer adder in leaderboard.js
