d7038cf3839d82fca2106a501a059cbd37a904d2
Jason Kaplan <jason.kaplan88@gmail.com>
2017-03-23 03:41:44 -0500

    Logo Added to Heatmap

--- webroot/js/heatmaps.js
+++ webroot/js/heatmaps.js
@@ -10,4 +10,12 @@ L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
 }).addTo(map);
 
+var legend2 = L.control({position: 'topright'}); 
+legend2.onAdd = function (map) {        
+    var div = L.DomUtil.create('div', 'info legend2 map-logo');
+    div.innerHTML = '<img src="../img/logo-light-blue-sm.png" style="width:125px; ">';     
+    return div;
+};      
+legend2.addTo(map);
+
 //heatmap layer adder in leaderboard.js
