05dc39f94a0b8879a877fc8f4bac14389f622d0d
John Liotta <john.liotta@gmail.com>
2017-05-20 14:53:34 -0500

    hamburger menu event icons

--- webroot/js/play_map.js
+++ webroot/js/play_map.js
@@ -1,7 +1,7 @@
 $('document').ready(function() {
 
     // svg tornado marker
-    var tornadoSVG = "<svg version='1.1' id='tornado' class='climacon climacon_tornado leaflet-marker-icon leaflet-zoom-animated leaflet-interactive leaflet-marker-draggable' viewBox='15 15 70 70'><g class='climacon_iconWrap climacon_iconWrap-tornado'><g class='climacon_componentWrap climacon_componentWrap-tornado'><path class='climacon_component climacon_component-stroke climacon_component-stroke_tornadoLine'd='M68.997,36.459H31.002c-1.104,0-2-0.896-2-1.999c0-1.104,0.896-2,2-2h37.995c1.104,0,2,0.896,2,2C70.997,35.563,70.102,36.459,68.997,36.459z'></path><path class='climacon_component climacon_component-stroke climacon_component-stroke_tornadoLine' d='M35.002,40.459h29.996c1.104,0,2,0.896,2,2s-0.896,1.999-2,1.999H35.002c-1.104,0-2-0.896-2-1.999C33.002,41.354,33.898,40.459,35.002,40.459z'></path><path class='climacon_component climacon_component-stroke climacon_component-stroke_tornadoLine' d='M39.001,48.458h21.998c1.104,0,1.999,0.896,1.999,1.999c0,1.104-0.896,2-1.999,2H39.001c-1.104,0-1.999-0.896-1.999-2C37.002,49.354,37.897,48.458,39.001,48.458z'></path><path class='climacon_component climacon_component-stroke climacon_component-stroke_tornadoLine' d='M47,64.456h5.999c1.104,0,2,0.896,2,1.999s-0.896,2-2,2H47c-1.104,0-2-0.896-2-2S45.896,64.456,47,64.456z'></path><path class='climacon_component climacon_component-stroke climacon_component-stroke_tornadoLine'd='M40.869,58.456c0-1.104,0.896-1.999,2-1.999h13.998c1.104,0,2,0.896,2,1.999c0,1.104-0.896,2-2,2H42.869C41.765,60.456,40.869,59.561,40.869,58.456z'></path></g></g></svg>";
+    var tornadoSVG = "<svg version='1.1' id='tornado' class='climacon climacon_tornado' viewBox='15 15 70 70'><g class='climacon_iconWrap climacon_iconWrap-tornado'><g class='climacon_componentWrap climacon_componentWrap-tornado'><path class='climacon_component climacon_component-stroke climacon_component-stroke_tornadoLine'd='M68.997,36.459H31.002c-1.104,0-2-0.896-2-1.999c0-1.104,0.896-2,2-2h37.995c1.104,0,2,0.896,2,2C70.997,35.563,70.102,36.459,68.997,36.459z'></path><path class='climacon_component climacon_component-stroke climacon_component-stroke_tornadoLine' d='M35.002,40.459h29.996c1.104,0,2,0.896,2,2s-0.896,1.999-2,1.999H35.002c-1.104,0-2-0.896-2-1.999C33.002,41.354,33.898,40.459,35.002,40.459z'></path><path class='climacon_component climacon_component-stroke climacon_component-stroke_tornadoLine' d='M39.001,48.458h21.998c1.104,0,1.999,0.896,1.999,1.999c0,1.104-0.896,2-1.999,2H39.001c-1.104,0-1.999-0.896-1.999-2C37.002,49.354,37.897,48.458,39.001,48.458z'></path><path class='climacon_component climacon_component-stroke climacon_component-stroke_tornadoLine' d='M47,64.456h5.999c1.104,0,2,0.896,2,1.999s-0.896,2-2,2H47c-1.104,0-2-0.896-2-2S45.896,64.456,47,64.456z'></path><path class='climacon_component climacon_component-stroke climacon_component-stroke_tornadoLine'd='M40.869,58.456c0-1.104,0.896-1.999,2-1.999h13.998c1.104,0,2,0.896,2,1.999c0,1.104-0.896,2-2,2H42.869C41.765,60.456,40.869,59.561,40.869,58.456z'></path></g></g></svg>";
 
     // svg hail marker
     var hailSVG = "<svg version='1.1' id='cloudHailAlt' class='climacon climacon_cloudHailAlt' viewBox='15 15 70 70'><g class='climacon_iconWrap climacon_iconWrap-cloudHailAlt'><g class='climacon_wrapperComponent climacon_wrapperComponent-hailAlt'><g class='climacon_component climacon_component-stroke climacon_component-stroke_hailAlt climacon_component-stroke_hailAlt-left'><circle cx='42' cy='65.498' r='2'></circle></g><g class='climacon_component climacon_component-stroke climacon_component-stroke_hailAlt climacon_component-stroke_hailAlt-middle'><circle cx='49.999' cy='65.498' r='2'></circle></g><g class='climacon_component climacon_component-stroke climacon_component-stroke_hailAlt climacon_component-stroke_hailAlt-right'><circle cx='57.998' cy='65.498' r='2'></circle></g><g class='climacon_component climacon_component-stroke climacon_component-stroke_hailAlt climacon_component-stroke_hailAlt-left'><circle cx='42' cy='65.498' r='2'></circle></g><g class='climacon_component climacon_component-stroke climacon_component-stroke_hailAlt climacon_component-stroke_hailAlt-middle'><circle cx='49.999' cy='65.498' r='2'></circle></g><g class='climacon_component climacon_component-stroke climacon_component-stroke_hailAlt climacon_component-stroke_hailAlt-right'><circle cx='57.998' cy='65.498' r='2'></circle></g></g><g class='climacon_wrapperComponent climacon_wrapperComponent-cloud'><path class='climacon_component climacon_component-stroke climacon_component-stroke_cloud' d='M63.999,64.941v-4.381c2.39-1.384,3.999-3.961,3.999-6.92c0-4.417-3.581-8-7.998-8c-1.602,0-3.084,0.48-4.334,1.291c-1.23-5.317-5.974-9.29-11.665-9.29c-6.626,0-11.998,5.372-11.998,11.998c0,3.549,1.55,6.728,3.999,8.924v4.916c-4.776-2.768-7.998-7.922-7.998-13.84c0-8.835,7.162-15.997,15.997-15.997c6.004,0,11.229,3.311,13.966,8.203c0.663-0.113,1.336-0.205,2.033-0.205c6.626,0,11.998,5.372,11.998,12C71.998,58.863,68.656,63.293,63.999,64.941z'></path></g></g></svg>";
@@ -201,28 +201,30 @@ $('document').ready(function() {
     };
 
     var inserted = '';
-    var popupTwo;
+    var popupTwo;var eventSVG;
     $.each(pendingLocations, function(i, v) {
         var stroke;
         var pendEvent = pendingEvents[i];
+        if (pendEvent === 'Tornado') {
+            eventSVG = tornadoSVG;
+            stroke = 'rgb(255, 51, 51)';
+        } else if (pendEvent === 'Hail') {
+            eventSVG = hailSVG;
+            stroke = 'rgb(61, 182, 239)'
+        } else if (pendEvent === 'Wind') {
+            eventSVG = windSVG;
+            stroke = 'rgb(255, 165, 0)'
+        }
         var rad = pendingRadius[i];
         var start = new Date(pendingDates[i]);
         var finish = new Date(pendingDatesEnd[i]);
         inserted += '<tr>'+
-                '<td>'+pendEvent+'</td>'+
+                '<td>'+eventSVG+'</td>'+
                 '<td>'+v[0].toFixed(2)+','+v[1].toFixed(2)+'</td>'+
                 '<td>'+rad+'mi</td>'+
                 '<td>'+(start.getMonth()+1)+'/'+start.getDate()+' '+start.getHours()+':00Z</td>'+
-                '<td>'+(Math.abs(finish - start) / 36e5)+'hrs</td>';
-        if (pendEvent === 'Tornado') {
-            stroke = 'rgb(255, 51, 51)';
-            $('.leaflet-popup-content-wrapper').css('background','rgb(255,51,51)')
-        } else if (pendEvent === 'Hail') {
-            stroke = 'rgb(61, 182, 239)'
-        } else if (pendEvent === 'Wind') {
-            stroke = 'rgb(255, 165, 0)'
-        }
-        inserted += '<td><button class="pendDelete" style="background-color: '+stroke+'">Delete</button></td></tr><hidden></hidden>';
+                '<td>'+(Math.abs(finish - start) / 36e5)+'hrs</td>'+
+                '<td><button class="pendDelete" style="background-color: '+stroke+'">Delete</button></td></tr><hidden></hidden>';
         popupTwo = L.popup({className: 'forecastPopup2'}).setContent('<div style="color:rgb(255,255,255)"><div class="row"><div class="col-md-12"><h4><strong>'+pendEvent+' Forecast</strong></h4></div></div><div class="row"><div class="col-md-6 pull-left"><span>inside a perimeter of</span></div><div class="col-md-6"><strong><span class="dateTime pull-right">'+Math.round(Math.PI * rad * rad).toLocaleString()+' mi<sup>2</sup></span></strong></div></div><div class="row"><div class="col-md-4 pull-left"><span>centered at</span></div><div class="col-md-8"><strong><span class="pull-right">'+dmsFormat(v[0],v[1])+'</span></strong></div></div><div class="row"><div class="col-md-4 pull-left"><span>between</span></div><div class="col-md-8"><strong><span class="pull-right">'+dateFormat(start)+'</span></strong></div></div><div class="row"><div class="col-md-4 pull-left">and</div><div class="col-md-8"><strong><span class="pull-right">'+dateFormat(finish)+'</span></strong></div></div></div><div class="row"><div class="col-md-12"><input class="toDelete" name="toDelete" type="hidden" value="'+pendingIDs[i]+'"/><input class="btn btn-primary deletePending" type="submit" value="Delete" style="background-color: '+stroke+'"/></div></div>');
         pending.push(L.circle(v, (rad * 1609.344), {
             color: stroke,
@@ -237,28 +239,31 @@ $('document').ready(function() {
     inserted = '';
     $.each(activeLocations, function(i, v) {
         var activeEvent = activeEvents[i];
+        if (activeEvent === 'Tornado') {
+            eventSVG = tornadoSVG;
+            stroke = 'rgb(255, 51, 51)'
+        } else if (activeEvent === 'Hail') {
+            eventSVG = hailSVG;
+            stroke = 'rgb(61, 182, 239)'
+        } else if (activeEvent === 'Wind') {
+            eventSVG = windSVG;
+            stroke = 'rgb(255, 165, 0)'
+        }
         var rad = activeRadius[i];
         var start = new Date(activeDates[i]);
         var finish = new Date(activeDatesEnd[i]);
         inserted += '<tr>'+
-                '<td>'+activeEvent+'</td>'+
+                '<td>'+eventSVG+'</td>'+
                 '<td>'+v[0].toFixed(2)+','+v[1].toFixed(2)+'</td>'+
                 '<td>'+rad+'mi</td>'+
                 '<td>'+(start.getMonth()+1)+'/'+start.getDate()+' '+start.getHours()+':00Z</td>'+
                 '<td>'+(Math.abs(finish - start) / 36e5)+'hrs</td></tr>';
-        if (activeEvents[i] === 'Tornado') {
-            stroke = 'rgb(255, 51, 51)'
-        } else if (activeEvents[i] === 'Hail') {
-            stroke = 'rgb(61, 182, 239)'
-        } else if (activeEvents[i] === 'Wind') {
-            stroke = 'rgb(255, 165, 0)'
-        }
-        active.push(L.circle(v, (activeRadius[i] * 1609.344), {
+        active.push(L.circle(v, (rad * 1609.344), {
             color: stroke,
             weight: 2,
             fillColor: '#262626',
             fillOpacity: 0.8
-        }).setStyle({className: 'clickThru'}).bindPopup('Active ' + activeEvents[i] + ' Forecast at <br><strong> ' + v[0] + ',' + v[1] + '</strong></br>Awaiting results...').openPopup());
+        }).setStyle({className: 'clickThru'}).bindPopup('Active ' + activeEvent + ' Forecast at <br><strong> ' + v[0] + ',' + v[1] + '</strong></br>Awaiting results...').openPopup());
     });
     $('#activeList').html(inserted);
 
